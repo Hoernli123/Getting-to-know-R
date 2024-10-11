500fa05053040ac6a693f23e6a5ac31009714a81
Janik Eichhorn <51954130+Hoernli123@users.noreply.github.com>
2024-10-11 14:23:15 +0200

    Getting to know R

---
+++ 0.1_into_to_R.R
@@ -0,0 +1,79 @@
+1+1
+
+4/2
+
+# R can multiply and make brackets!
+(2+2)*3
+
+# R can tell the truth!
+2==2
+2==3
+
+2>3
+2<3
+
+2 >= 3 # this works!
+2 => 3 # this does not work!
+
+2 <= 3 # this works!
+2 =< 3 # this does not work!
+
+# Functions
+
+getwd()
+
+sqrt(9)
+
+log(9)
+
+
+# Assignments
+
+x <- 3
+x
+x + 3
+y <- 10 - 6
+y <- x
+sqrt(x)
+sqrt(y)
+
+# Case-sensitive
+
+x
+X
+
+# challenge 1
+
+_age <- 3
+.mass <- 3
+2widths <- 3
+celsius2kelvin <- 3
+
+#challenge 2
+
+mass <- 47.5
+age <- 122
+mass <- mass *2.3
+age <- age - 20
+mass > age
+
+
+# Vectors
+vec1 <- c(1, 2, 3, 4, 5)
+vec2 <- 1:5
+vec3 <- c("dog", "cat", "rabbit")
+vec4 <- c("dog", 1, 3)
+vec5 <- c(TRUE, FALSE, TRUE)
+
+
+# Loading package
+library(gapminder)
+library(ggplot2)
+library(dplyr)
+browseVignettes(package = "dplyr")
+
+
+
+
+
+
